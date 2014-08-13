ffd5868494cf50cecf172202d92132af78c26b0c
Samkiff <samantha.midkiff@gmail.com>
2014-08-12 18:51:08 -0700

    Add span class draft

---
+++ classes/python/span.py
@@ -0,0 +1,29 @@
+class Span:
+
+#[startTime, endTime]
+
+    def __init__(self):
+
+    def span spanIncrEnd( span ):
+    # Add a week? for to span for searching
+    # Parameters: Span(startTime, EndTime)
+    # Returns: Span(startTime, EndTime)
+
+    def span spanDecrEnd( span ):
+    # Decrease a week? for to span for searching
+    # Parameters: Span(startTime, EndTime)
+    # Returns: Span(startTime, EndTime)
+    # precond: Span > 1 Week
+
+    def void printSpans():
+    # Parameters: List of Spans(startTime, EndTime)
+    # Returns:  void
+    # Print spans startTime and endTime
+
+    def span Span( dateTime start, dateTime end )
+
+    def int compareTo( span, span )
+    # Parameters: List of Spans(startTime, EndTime)
+    # Returns: -1, 1, 0 for <, >, =
+
+
