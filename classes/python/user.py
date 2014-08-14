38b31501445b08eb903a639b1f3b5d3b01c5f30f
Samkiff <samantha.midkiff@gmail.com>
2014-08-13 17:49:20 -0700

    Create user.py

---
+++ classes/python/user.py
@@ -0,0 +1,10 @@
+class User:
+
+	def user user(string firstName, string lastName, string email, span [] events)
+		# Parameters: string firstName, string lastName, string email, span [] events
+		# Returns: user
+		# determine freeTimes from events?
+
+	def printUser (user a)
+		# Parameters: user
+		# Returns: 
