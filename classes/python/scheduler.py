fa1c4d174aa9603fb5a83f9e8a1edbcdf137bf21
tlewismedia <tlewismedia@gmail.com>
2014-08-16 15:21:23 -0700

    changes to findFreeTimes

--- classes/python/scheduler.py
+++ classes/python/scheduler.py
@@ -2,7 +2,7 @@
 # Responsible for scheduling logic, finding free times / users
 
 # includes here
-
+import datetime
 
 class Scheduler:
 
@@ -11,6 +11,39 @@ class Scheduler:
 
     def __init__(self):
         # init
+
+
+    def findFreeTimes( winStart, winEnd, users ):
+        ######################################################################
+        # returns: a list of spans
+        # parameters: a list of users, [ window start, window end ]
+        # precond: list of users > 0, window start is in future,
+        # window end is reasonable
+        # note: needs to be called with keyword argument like findFreeTime( winStart = 1, winEnd = 2, users=users)
+
+        events = []
+        freeTimes = []
+
+
+        if not winStart:
+            winstart = datetime.datetime.now()
+        if not winEnd:
+            winEnd = winStart + timedelta(days=WINDOWDEFAULT)
+        
+
+        events = makeEventsLst( users )
+
+        # add empty space to freeTimes 
+        start = winStart
+        for i in xrange(1, events.length - 1):
+            freeTimes[] = Span( start, events[i].start)
+            start = events[i].end
+        freeTimes[] = Span( start, winEnd)
+
+
+        return freeTimes
+
+
 
 
     def findFreeUsers( winStart, winEnd ):
@@ -47,3 +80,22 @@ class Scheduler:
         # parameters: a list of users, [ window start, window end ]
         # precond: list of users > 0, window start is in future, window end
         # is reasonable
+        events = []
+
+        # add calender events
+
+
+
+        # add courses
+
+
+
+        # consolidate and sort
+
+
+
+        return events
+
+
+
+
