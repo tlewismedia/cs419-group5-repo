a30ad9dd100019319cb1c13999c5bde298b304e8
tlewismedia <tlewismedia@gmail.com>
2014-08-17 13:51:21 -0700

    refactored findFreeTimes, see psuedocode for general flow

--- classes/php/scheduler.php
+++ classes/php/scheduler.php
@@ -12,13 +12,20 @@ require_once "classes/MyDB.php";
 
 define('DEBUG', true);
 
-
-
-
 class Scheduler {
 
 	define("SPANUNIT", 1);
 	define("WINDOWDEFAULT", 30); //30 days
+	
+	$_DAYS = array(
+    "M" => 0,
+    "T" => 1,
+    "W" => 2,
+    "R" => 3,
+    "F" => 4,
+    "S" => 5,
+    "N" => 6
+	);
 
 	$_service;   // google cal api service object
 	
@@ -30,7 +37,7 @@ class Scheduler {
 
 	function findFreeUsers( winStart, winEnd )
 	{
-		/**
+		/*****************************************************************************
 		* returns: a list of spans
 		* parameters:  window start, window end 
 		* precond: list of users > 0, window start is in future, window end is reasonable
@@ -40,7 +47,7 @@ class Scheduler {
 
 	function isConflict( span, span )
 	{
-		/**
+		/*****************************************************************************
 		* returns: true if no conflicts
 		* parameters: span interval, span list events
 		* precond: list of users > 0, span length > 0
@@ -50,7 +57,7 @@ class Scheduler {
 
 	function checkForConflicts( $interval, $events )
 	{
-		/**
+		/*****************************************************************************
 		* returns: true if no conflicts
 		* parameters: span interval, span list events
 		* precond: list of users > 0, span length > 0
@@ -66,17 +73,60 @@ class Scheduler {
 
 	}
 
-	function findFreeTimes( winStart, winEnd, users )
+	function findFreeTimes( $winStart, $winEnd, $users )
 	{
-		/**
+		/*****************************************************************************
 		* returns: a list of spans
 		* parameters: a list of users, [ window start, window end ]
 		* precond: list of users > 0, window start is in future, window end is reasonable
 		*/
 
-		//get events from google calendars
+		$events = array();
+		$freeTimes = array();
+
+		//set win defaults
+		if (!$winStart) $winStart = new DateTime();
+		if (!$winEnd) $winEnd = $winStart->add(new DateInterval('P'.WINDOWDEFAULT.'D'));
+
+		//make event list
+		$events = makeEventList( $users )
+
+		// make free time list
+		$freeTimes = makeFreeList( $winStart, $winEnd, $events);
+
+		return $freeTimes
+		
+	} //findFreeTimes()
+		/*****************************************************************************
+		* returns: Span[] (free times)
+		* parameters: dateTime winStart, dateTime winEnd,  Span[] events
+		*/
+
+	function makeFreeList( $winStart, $winEnd, $events ){
+
+		# add empty space to freeTimes 
+		$start = $winStart
+		for ($i = 1, $events.length() - 1, $i++){
+		   $freeTimes[] = Span( $start, $events[i].getStart());
+		   $start = $events[i].getEnd();
+		}
+		$freeTimes[] = Span( $start, $winEnd);
+		
+
+		return $freeTimes
+
+
+	}
+
+	function getCalEvents(&$events, $users){
+		/*****************************************************************************
+		* returns: void
+		* parameters: event list, list of users
+		*/
 		// http://stackoverflow.com/questions/17553273/how-to-finish-this-google-calendar-api-v3-freebusy-php-example/17991286#17991286
-		$eventList = array();
+		foreach ($users as $user){
+			$userNames[] = $user.getEmail();
+		}
 
 		$freebusy = new Google_FreeBusyRequest();
 		$freebusy->setTimeMin($winStart);
@@ -84,7 +134,7 @@ class Scheduler {
 		$freebusy->setGroupExpansionMax(10);
 		$freebusy->setCalendarExpansionMax(10);
 		
-		$freebusy->setItems = $users;
+		$freebusy->setItems = $userNames;
 		$result = $service->freebusy->query($freebusy);
 		$cals = $result->getCalendars();
 
@@ -92,13 +142,45 @@ class Scheduler {
 		foreach ($cals as $cal){
 			$busys = $cal->getBusy();
 			foreach ($busys as $busy){ 
-				$events[] = new $span($busy.start, $busy.end);
+				$events[] = new $span($busy['start'], $busy['end']);
 			}
 		}
+	}
 
+	function makeEventList( $winStart, $winEnd, $users )
+	{
+		/*****************************************************************************
+		* returns: a list of spans (courses and calendar events of users)
+		* parameters: a list of users, [ window start, window end ]
+		* precond: list of users > 0, window start is in future, window end is reasonable
+		*/
+		$events = [];
+	
+		getCalEvents($events, $users);
+		getCourseEvents($events, $users);
 
-		//get events from courses DB for each user
+		//consolidateSpans($events)
+
+		//sortSpans($events)
+
+		return $events;
+		
+	} //makeEventList
+
+	function getCourseEvents(&$events, &$users){
+	{
+		/*****************************************************************************
+		* returns: void
+		* parameters: refernce to event list, list of users
+		*/
+	//get events from courses DB for each user
 		$db = new MyDB();
+
+
+		//for each user
+		//		for each class
+		//			for each day of that class
+
 		
 		foreach ($users as $user){
 			$sel = "SELECT p.firstName, c.days, c.startDate, c.endDate, c.startTime, c.endTime   FROM Courses c
@@ -107,26 +189,49 @@ class Scheduler {
           		WHERE p.email = '".$user->email()."';";
 
 
-			$result = $db->query($sel);  //result should have 
+			$result = $db->query($sel);   
 
-			while( $row = $result->fetchArray()){
-    			for each row, get the days {
-    				for each letter in days
-    					day = dayMap[letter]
-    					
-    					curDate = walkToFirstDateWithDay(row.startDate, day)  //returns a date
-    					
-						while (curDate <= row.endDate) {
-							$eventDateTimeStart = convertToDateTime (start, row.startTime)
-							$eventDateTimeEnd = convertToDateTime (start, row.endTime)
-	    					$events[] = new event( $eventDateTimeStart, $eventDateTimeStart )
-	    					curDate.addDays(7); //days in a week   may need to make function
-	    				} //while
-    			} // foreach
+			while( $row = $result->fetchArray()){  //fetches each class row
+    			
+ 				//get the days
+ 				$daysArr = str_spli($row.days);
+ 				
+ 				//add course span for each day
+ 				foreach ($daysArr as $letter){  //for each day of that class
+ 					$day = DAYMAP[$letter];
+ 					
+ 					$curDate = getFirstDateForDay($row.startDate, $day)  //returns a date
+ 					
+					while (curDate <= row.endDate) {  //add all spans for that day within the window
+						$eventDateTimeStart = convertToDateTime (curDate, row.startTime)  //specify thye time
+						$eventDateTimeEnd = convertToDateTime (curDate, row.endTime)
+    					$events[] = new Span( $eventDateTimeStart, $eventDateTimeStart )
+
+    					$curDate->add(new DateInterval('P7D')); //days in a week 
+    				} //while
   			} //while
 		} //foreach
-	} //makeEventList()
+	} //getCourseEvents
 
+  /****************************************************************************
+	* returns: a date marking the first of reoccuring days
+	* parameters: startDate (of course), day
+	* precond: day must be in DAYMAP
+	*
+	* ex: If the class is on M and W starting on 08/14/2014, we can use this function to
+	*     find the first W.
+	*/
+	function getFirstDateforDay( $startDate, $day){
+		$startDay = getDay($startDate);
+		$day1 = $_DAYS[$startDay]; //should return int (0-6)
+		$day2 = $_DAYS[$day];
+		$daysToAdd = (($day2 + $_DAYS.length()) - $day1) % $_DAYS.length();  //find num days to add
+
+
+		$startDate->add(new DateInterval('P'.$daysToAdd.'D'));
+
+		return $startDate;
+	} //getFirstDateforDay
 }
 
 ?>
