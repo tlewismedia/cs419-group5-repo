a4579a8dc39974d1c2ebd20bcce36ef8af034021
Samkiff <samantha.midkiff@gmail.com>
2014-08-18 09:56:35 -0700

    Update user.py

--- classes/python/user.py
+++ classes/python/user.py
@@ -1,10 +1,17 @@
 class User:
 
-	def user user(string firstName, string lastName, string email, span [] events)
+	def __init__(self, firstName, lastName,  email, events):
 		# Parameters: string firstName, string lastName, string email, span [] events
 		# Returns: user
 		# determine freeTimes from events?
+		self.firstName = firstName
+		self.lastName = lastName
+		self.email = email
+		self.events = events
 
-	def printUser (user a)
+	def printUser (self)
 		# Parameters: user
 		# Returns: 
+		print self.firstName 
+		print self.lastName 
+		print self.email 
