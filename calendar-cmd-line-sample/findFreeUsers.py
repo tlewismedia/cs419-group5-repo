570e856a61c531c974ffb07de68d198e848c0af9
Samkiff <samantha.midkiff@gmail.com>
2014-08-22 01:00:48 -0700

    changes to span, findFreeUsers, example

--- calendar-cmd-line-sample/findFreeUsers.py
+++ calendar-cmd-line-sample/findFreeUsers.py
@@ -1,16 +1,5 @@
-
-"""Command-line skeleton application for Calendar API.
-Usage:
-  $ python sample.py
-
-You can also get help on all the command-line flags the program understands
-by running:
-
-  $ python sample.py --help
-
-"""
-from span import Span
-import time
+from span import *
+import datetime
 import httplib2
 import os
 import sys
@@ -26,15 +15,9 @@ from oauth2client import tools
 
 
 # CLIENT_SECRETS is name of a file containing the OAuth 2.0 information for this
-# application, including client_id and client_secret. You can see the Client ID
-# and Client secret on the APIs page in the Cloud Console:
-# <https://cloud.google.com/console#/project/1022620417954/apiui>
+# application
 CLIENT_SECRETS = os.path.join(os.path.dirname(__file__), 'client_secrets.json')
 
-# Set up a Flow object to be used for authentication.
-# Add one or more of the following scopes. PLEASE ONLY ADD THE SCOPES YOU
-# NEED. For more information on using scopes please see
-# <https://developers.google.com/+/best-practices>.
 FLOW = client.flow_from_clientsecrets(CLIENT_SECRETS,
   scope=[
       'https://www.googleapis.com/auth/calendar',
@@ -44,34 +27,21 @@ FLOW = client.flow_from_clientsecrets(CLIENT_SECRETS,
 
 
 def getCalEvents(user, startTime, endTime):
-  # Parse the command-line flags.
 
-  # If the credentials don't exist or are invalid run through the native client
-  # flow. The Storage object will ensure that if successful the good
-  # credentials will get written back to the file.
   storage = file.Storage('sample.dat')
   credentials = storage.get()
   if credentials is None or credentials.invalid:
     credentials = tools.run_flow(FLOW, storage, flags)
 
-  # Create an httplib2.Http object to handle our HTTP requests and authorize it
-  # with our good Credentials.
   http = httplib2.Http()
   http = credentials.authorize(http)
 
-  # Construct the service object for the interacting with the Calendar API.
   service = discovery.build('calendar', 'v3', http=http)
 
   try:
-    print "Success! Now add code here."
     page_token = None
     while True:
       calendar_list = service.calendarList().list(pageToken=page_token).execute()
-      for calendar_list_entry in calendar_list['items']:
-        print calendar_list_entry['summary']
-
-      print "getting freebusy"
-      
 
       freebusy_query = {
         "timeMin" : startTime,
@@ -88,9 +58,9 @@ def getCalEvents(user, startTime, endTime):
       busyList = []
       events = response['calendars'][user]['busy']
       for e in events:
-        print e['start'] + " " + e['end']
-        s = deGoogle (e['start'])
-        en = deGoogle (e['end'])
+        #print e['start'] + " " + e['end']
+        s = timeConv.deGoogle (e['start'])
+        en = timeConv.deGoogle (e['end'])
         x = Span(s, en)
         busyList.append(x)
       
@@ -104,28 +74,17 @@ def getCalEvents(user, startTime, endTime):
     print ("The credentials have been revoked or expired, please re-run"
       "the application to re-authorize")
 
-def deGoogle(timeVal):
-    t = time.strptime(timeVal, "%Y-%m-%dT%H:%M:%SZ")
-    return t
 
-def printSpans(spans):
-    # Parameters: List of Spans(startTime, EndTime) or print span
-    # Returns:  void
-    # Print spans startTime and endTime
-    for span in spans:
-        printLegible(span.start)
-        printLegible(span.end)
-
-def printLegible( timeVal ):
-    print time.strftime("%a, %d %b %Y %H:%M", timeVal)
-
-def findFreeUsers(emails, reqStart, reqEnd):
+def findFreeUsers(emails, span):
     availableUsers =[]
+    reqStart = timeConv.forGoogle(span.start)
+    reqEnd = timeConv.forGoogle(span.end)
     for email in emails:
         free = True
-        eventList = getCalEvents(email, beg, done)
+        eventList = getCalEvents(email, reqStart, reqEnd)
+        Span.printSpans(eventList)
         for event in eventList:
-            if Span.isConflict (eventStart, eventEnd, reqStart, reqEnd):
+            if Span.isConflict (event, span):
                 free = False
         if free == True:
             availableUsers.append(email)
@@ -134,12 +93,11 @@ def findFreeUsers(emails, reqStart, reqEnd):
 if __name__ == '__main__':
     email = []
     email.append(sys.argv[1])
-    a = sys.argv[2]
-    b = sys.argv[3]
-    start = time.strptime(a, "%d %b %Y %H:%M")
-    end = time.strptime(b, "%d %b %Y %H:%M")
-
-    beg = time.strftime("%Y-%m-%dT%H:%M:%S.000-07:00", start)
-    done = time.strftime("%Y-%m-%dT%H:%M:%S.000-07:00", end)
-    print findFreeUsers(email, beg, done)
+    email.append(sys.argv[2])
+    start = timeConv.stringtodt(sys.argv[3])
+    end = timeConv.stringtodt(sys.argv[4])
+    reqTime = Span(start, end)
+    freeUsers = findFreeUsers(email, reqTime)
+    for user in freeUsers:
+        print user
 
--- classes/python/scheduler.py
+++ classes/python/scheduler.py
@@ -10,13 +10,13 @@ class Scheduler:
     SPANUNIT = 1  # min
     WINDOWDEFAULT = 30  # days
     DAYS = {
-        "M": 0,
-        "T": 1,
-        "W": 2,
-        "R": 3,
-        "F": 4,
-        "S": 5,
-        "N": 6
+        "M": 1,
+        "T": 2,
+        "W": 3,
+        "R": 4,
+        "F": 5,
+        "S": 6,
+        "N": 0
     }
 
     # def __init__(self):
@@ -61,39 +61,77 @@ class Scheduler:
         
         return $freeTimes
 
-    def getCalEvents( events, users, service):
+    def getCalEvents( users, startTime, endTime):
         ######################################################################
         # returns: void
         # parameters: event list, list of users, gCal service object
-        calendar_list = service.calendarList().list(pageToken=page_token).execute()
 
-        freebusy_query = {
-            "timeMin" : '2014-08-10T10:00:00.000-07:00',
-            "timeMax" : '2014-08-16T10:00:00.000-07:00',
-            "items" :[
-              {
-                "id" : 'fakeymcfakerson789@gmail.com'
-              }
-            ]
-          }
-          request = service.freebusy().query(body=freebusy_query)
-          response = request.execute()
+        storage = file.Storage('sample.dat')
+        credentials = storage.get()
+        if credentials is None or credentials.invalid:
+          credentials = tools.run_flow(FLOW, storage, flags)
 
-          
-          events = response['calendars']['fakeymcfakerson789@gmail.com']['busy']
-          for e in events:
-            print e['start'] + " " + e['end']
+        http = httplib2.Http()
+        http = credentials.authorize(http)
 
-            # create new spans and add to events
+        service = discovery.build('calendar', 'v3', http=http)
+
+        try:
+          page_token = None
+          while True:
+            calendar_list = service.calendarList().list(pageToken=page_token).execute()
+
+            freebusy_query = {
+              "timeMin" : startTime,
+              "timeMax" : endTime,
+              "items" :[
+                {
+                  "id" : user
+                }
+              ]
+            }
+            request = service.freebusy().query(body=freebusy_query)
+            response = request.execute()
+
+            busyList = []
+            events = response['calendars'][user]['busy']
+            for e in events:
+              #print e['start'] + " " + e['end']
+              s = timeConv.deGoogle (e['start'])
+              en = timeConv.deGoogle (e['end'])
+              x = Span(s, en)
+              busyList.append(x)
+            
+
+            page_token = calendar_list.get('nextPageToken')
+            if not page_token:
+              return busyList
 
 
-    def findFreeUsers( winStart, winEnd ):
+        except client.AccessTokenRefreshError:
+          print ("The credentials have been revoked or expired, please re-run"
+            "the application to re-authorize")
+
+
+    def findFreeUsers( emails, span ):
         ######################################################################
-        # returns: a list of spans
+        # returns: a list of available users
         # parameters: a list of users, [ window start, window end ]
         # precond: list of users > 0, window start is in future,
         # window end is reasonable
-
+        availableUsers =[]
+        reqStart = timeConv.forGoogle(span.start)
+        reqEnd = timeConv.forGoogle(span.end)
+        for email in emails:
+            free = True
+            eventList = getCalEvents(email, reqStart, reqEnd)
+            Span.printSpans(eventList)
+            for event in eventList:
+                if Span.isConflict (event, span):
+                    free = False
+            if free == True:
+                availableUsers.append(email)
+        return availableUsers
 
     def isConflict( span, span ):
         ######################################################################
@@ -136,57 +174,88 @@ class Scheduler:
 
 
     def getCourseEvents( events, users ):
-        ######################################################################
-        # returns: void
-        # parameters: refernce to event list, list of users
-          
-        db = MyDB()
-        conn = db.connect()
-        c = conn.cursor()
-        
-        query = """SELECT p.firstName, c.days, c.startDate, c.endDate, c.startTime, c.endTime   FROM Courses c
-                INNER JOIN CoursesProfs cp ON cp.cid = c.prof
-                INNER JOIN Profs p ON p.email = cp.pid
-                WHERE p.email = '"+$user->email()+"';"""
-        c.execute(query)
-        
-
-        while( row = c.fetchArray()):
-
-            # get the days
-            daysArr = list(row.days)
-
-            # add course span for each day
-            for letter in daysArr:  #for each day of that class
-                day = DAYMAP[letter]
-                
-                curDate = getFirstDateForDay(row.startDate, day)  #returns a date
-                
-                while (curDate <= row.endDate):  #add all spans for that day within the window
-                    eventDateTimeStart = convertToDateTime (curDate, row.startTime)  #specify thye time
-                    eventDateTimeEnd = convertToDateTime (curDate, row.endTime)
-                    events[] = new Span( eventDateTimeStart, eventDateTimeStart )
-
-                    curDate = curDate + datetime.timedelta(days=7)  # days in a week 
-                
-
-
-        # print c.fetchall()
-
-
-    def getFirstDateforDay( startDate, day):
     ######################################################################
-    # returns: a date marking the first of reoccuring days
-    # parameters: startDate (of course), day
+    # returns: void
+    # parameters: refernce to event list, list of users
+      
+    db = MyDB()
+    conn = db.connect()
+    c = conn.cursor()
+   
+    # http://stackoverflow.com/questions/283645/python-list-in-sql-query-as-parameter
+    placeholder= '?' 
+    placeholders= ', '.join(placeholder for unused in users)
+
+    query = """SELECT  c.days, c.startDate, c.endDate, c.startTime, c.endTime   FROM Courses c
+            INNER JOIN CoursesProfs cp ON cp.cid = c.prof
+            INNER JOIN Profs p ON p.email = cp.pid
+            WHERE p.email IN (%s)""" % placeholders
+
+    results = c.execute(query, users)
+    for row in results:
+        # print row
+       
+        # format data
+        daysArr = list(row[0])
+        startDate = datetime.datetime.strptime(row[1], "%x");
+        endDate = datetime.datetime.strptime(row[2], "%x"); 
+       
+        startTime = str(row[3])
+
+        if len(startTime) == 3: #need four digits
+            startTime = '0'+startTime
+        startTime = datetime.time( int(startTime[0:2]), int(startTime[2:4]))
+  
+        endTime = str(row[4])
+        if len(endTime) == 3: #need four digits
+            endTime = '0'+endTime
+        endTime = datetime.time( int(endTime[0:2]), int(endTime[2:4]))
+
+        # add course span for each day
+        for letter in daysArr:  #for each day of that class
+            
+            curDate = getFirstDateForDay(startDate, letter)  #returns a datetime
+            
+            while (curDate <= endDate):  #add all spans for that day within the window
+                
+                date = curDate.date()
+
+                # print type(date)
+                # print type(startTime)
+
+                year = date.year
+                month = date.month
+                day = date.day
+                shour = startTime.hour
+                sminute = startTime.minute
+                ehour = endTime.hour
+                eminute = endTime.minute
+
+                eventDateTimeStart = datetime.datetime.combine(datetime.date(year, month, day), datetime.time( shour, sminute))  
+                eventDateTimeEnd = datetime.datetime.combine(datetime.date(year, month, day), datetime.time( ehour, eminute))  
+                
+                newSpan = Span( eventDateTimeStart, eventDateTimeEnd )
+
+                events.append(newSpan)
+
+                curDate = curDate + datetime.timedelta(days=7)  # days in a week 
+                
+
+
+    def getFirstDateForDay( startDate, letter):
+    ######################################################################
+    # returns: dateTime for first occurance of day (name) after the startDate
+    # parameters: dateTime startDate (of Course), day (letter)
     # 
     # ex: If the class is on M and W starting on 08/14/2014, we can use this function to
     #     find the first W.
-    startDay = getDay(startDate)  # TODO: getday
-    day1 = DAYS[startDay] #should return int (0-6)
-    day2 = DAYS[day]
-    daysToAdd = ((day2 + _DAYS.length()) - day1) % DAYS.length()  #find num days to add
+   
+    day1 = int(datetime.datetime.strftime( startDate, "%w"))
+    day2 = DAYS[letter]
 
-    startDate = startDate + datetime.timedelta(days=daysToAdd)
+    delta = (day2 + 7 - day1) % 7  #find num days to add
+    newDate = startDate + datetime.timedelta(days=delta)
 
-    return startDate;
+    # print newDate
 
+    return newDate
