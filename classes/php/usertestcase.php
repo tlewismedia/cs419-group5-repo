5163c6a0303e54f0214f26fc496514308e807eb6
kodalik <krishnakodali06@gmail.com>
2014-08-25 21:12:16 -0400

    Create usertestcase.php

---
+++ classes/php/usertestcase.php
@@ -0,0 +1,13 @@
+<?php
+include "User.php";
+/* Test Case for  User class
+*
+*
+*/
+$obj	=	new User();
+
+### testing isConflict ####
+$result = $obj->printUser('Neeraj','Chandola','chandola.neeraj@gmail.com');
+?>
+
+
