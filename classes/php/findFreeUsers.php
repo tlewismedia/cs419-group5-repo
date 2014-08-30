02d4079751b9d05aedb67a1aba44e2cb056f1d45
kodalik <krishnakodali06@gmail.com>
2014-08-30 08:31:33 -0400

    Update findFreeUsers.php

--- classes/php/findFreeUsers.php
+++ classes/php/findFreeUsers.php
@@ -1,11 +1,9 @@
 <?php
 ////////////////////////////////////////////////////
-// SPAN - PHP Findfreeuser class
+//  - PHP Findfreeuser class
 //
 // Version 1.01
 //
-// Define an SPAN class that can be used to connect
-// and communicate with google calender api.
 //
 // Author: Krishna Kodali
 //
@@ -24,7 +22,11 @@ class Findfreeuser extends Span{
              {
                  $free = true;
                  $eventList = $this->getCalEvents($email, $beg, $done);
-                 if (Span::isConflict ($eventStart, $eventEnd, $reqStart, $reqEnd))
+				 $span1['start'] = $eventStart;
+				 $span1['end'] = $eventEnd;
+				 $span2['start'] = $reqStart;
+				 $span2['end'] = $reqEnd;
+                 if (Span::isConflict ($span1, $span2))
                  {
                      $free = false;
                  }
@@ -38,12 +40,4 @@ class Findfreeuser extends Span{
          }
     }
 	
-	/* Method to get calender events via api
-	*
-	*/
-	
-	public function getCalEvents($user, $startTime, $endTime)
-    {
-        // in this method we call api to check user events for selected user emial and give start and end time
-    }
-}
+
